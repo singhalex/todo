485c5c278552f76c09e1925d1c7740216812d612
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-01-17 19:52:37 -0800

    Create task and project objects
    Factory functions created in order to facilitate the creation of tasks
    and projects by the user.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import './style.css';
 import { formatDistance, subDays } from 'date-fns';
 
-console.log(formatDistance(subDays(new Date(), 6), new Date(), { addSuffix: true }));
+const Task = (title, description, dueDate, priority) => {
+  const returnTitle = () => title;
+  const returnDescription = () => description;
+  const returnDueDate = () => dueDate;
+  const returnPriority = () => priority;
+
+  return {
+    returnTitle, returnDescription, returnDueDate, returnPriority,
+  };
+};
+
+const Project = (title) => {
+  const returnTitle = () => title;
+  const taskList = [];
+  const returnTaskList = () => taskList;
+
+  const addTask = (task) => {
+    taskList.push(task);
+  };
+
+  return { returnTitle, addTask, returnTaskList };
+};
+
+const firstProject = Project('Main');
+const firstTask = Task('Clean room', 'Make sure to clean the walls', 'tomorrow', 'High');
+
+firstProject.addTask(firstTask);
+
+console.log(firstProject);
+console.log(firstProject.returnTaskList());
+console.log(firstTask);
